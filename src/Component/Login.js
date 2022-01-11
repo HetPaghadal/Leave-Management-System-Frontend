704c0a91693d24dd040469b48ca9bf8a020e2a60
Het Paghadal <het.paghadal@squadstack.com>
2022-01-11 18:54:25 +0530

    GoogleAuth2.0

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
-// require('dotenv').config();
 
 const clientId = process.env.REACT_APP_CLIENT_KEY;
 
 function Login() {
   const navigate = useNavigate();
   const onSuccess = (res) => {
-    // console.log('Login Success:', res.profileObj);
     navigate('/done');
   };
 
-  const onFailure = (res) => {
-    // console.log('Login failed:', res);
-  };
+  const onFailure = (res) => {};
 
   return (
     <div>
-      <h1>Welcome To Squadstack!</h1>
+      <h1>Welcome To Squadstack Leave Tracker!</h1>
       <GoogleLogin
         clientId={clientId}
         buttonText="Login with Google"
         onSuccess={onSuccess}
         onFailure={onFailure}
         cookiePolicy="single_host_origin"
-        // isSignedIn={true}
+        isSignedIn={true}
       />
     </div>
   );
