ac6c2de2c1307c9d467e9a459b970948e573b8b5
Het Paghadal <het.paghadal@squadstack.com>
2022-01-13 16:20:31 +0530

    Token

--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -6,8 +6,44 @@ const axios = require('axios');
 const FormData = require('form-data');
 
 const clientId = process.env.REACT_APP_CLIENT_KEY;
+const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+let ref;
 
 function Homepage() {
+  function CheckAccesstoken(accessToken) {
+    const URL = `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`;
+    let res = 0;
+    fetch(URL).then((response) => {
+      // console.log(response.status);
+      if (true) res = 1;
+    });
+    return res;
+  }
+  const GenerateAccesstoken = (refreshToken) => {
+    const data = new FormData();
+    data.append('client_id', clientId);
+    data.append('client_secret', clientSecret);
+    data.append('refresh_token', refreshToken);
+    data.append('grant_type', 'refresh_token');
+
+    const config = {
+      method: 'post',
+      url: 'https://oauth2.googleapis.com/token',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        // console.log(response.data);
+      })
+      .catch(function (error) {
+        // console.log(error);
+      });
+  };
+
   const navigate = useNavigate();
   const onSuccess = (res) => {
     const Auth = res.code;
@@ -16,7 +52,7 @@ function Homepage() {
     const data = new FormData();
     data.append('code', Auth);
     data.append('client_id', clientId);
-    data.append('client_secret', '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');
+    data.append('client_secret', clientSecret);
     data.append('grant_type', 'authorization_code');
     data.append('redirect_uri', 'http://localhost:3000');
 
@@ -31,12 +67,11 @@ function Homepage() {
 
     axios(config)
       .then(function (response) {
-        // console.log(response.data.refresh_token);
+        ref =
+          'ya29.A0ARrdaM-zhvpTcrVbeavwTN_789fi6Yi0LxZoxNK-ZiAi9xFUBp6Ku0Vi-wgLyjtSEICiIQ1JG5UP2MswOfuw00FAKt7-Ywu4a73VREwL2InThm3NxYLD5NuhEGqGg-IbuVGY_jADwdwOvmevmK5Z7U1Cm7sI';
+        // console.log(CheckAccesstoken(ref));
       })
-      .catch(function (error) {
-        // console.log(error);
-      });
-
+      .catch(function (error) {});
     navigate('/Dashboard');
   };
 
